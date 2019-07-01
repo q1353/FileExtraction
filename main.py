6ddbd83f322d87562fcd67fdfb51cce5b6cd5498
yasuyukihoshibaWin10 <q1353tq@gmail.com>
2019-07-01 15:42:08 +0900

    '完成'

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 from glob import glob
 from datetime import datetime
 
-#対象とするディレクトリリストを取得
-path = r'\\pluto\d(pluto)\●安全衛生\安全(大阪ｻｲﾄ)\交通\４８期'
+#検査対象ディレクトリを入力
+print ('検査対象のディレクトリパスを入力してください')
+path = input()
 
 #ディレクトリのみを取得する関数
 def getdirs(path):
@@ -18,11 +19,42 @@ directry_list = getdirs(path)
 directry_list_num = len (directry_list)
 
 #検査対象ファイルリスト（2次元）空の作成
-file_lsit = [[] for i in range(directry_list_num)]
+file_list = [[] for i in range(directry_list_num)]
 
-n = 0
-for n in directry_list_num:
+#検査対象ファイルリストの作成
+for n in range(directry_list_num):
     for directry in directry_list:
-        file_lsit[n].append(glob(os.path.join(path , directry)))
+        file_list[n].append(glob(os.path.join(path , (directry + '\*'))))
 
-print (file_lsit)
+#検査対象ファイルリストの全ファイルの更新日時(エポック秒)を取得 ※datetime.fromtimestamp()でエポック秒をタイムスタンプに変換
+#検査対象ファイルの更新時間リスト（空）の作成
+updatetime_list = [[] for i in range(directry_list_num)]
+
+#検査対象ファイルの更新時間リストの作成
+for n in range(directry_list_num):
+    for file_path in file_list[0][n]:
+        updatetime_list[n].append(os.path.getctime(file_path))
+
+#検査対象ファイルの更新時間リストそれぞれから最新更新時間を取得
+#最新更新時間リスト（空）の作成
+updatetime_new = []
+
+#最新更新時間リストの作成
+for n in range(directry_list_num):
+    updatetime_new.append(max(updatetime_list[n]))
+
+#最新更新時間リストのエポック秒を年/月/日 時間：分 に変換
+updatetime_new_alt = []
+for n in range(directry_list_num):
+    updatetime_new_alt.append(datetime.fromtimestamp(updatetime_new[n]).strftime("%Y/%m/%d %H:%M"))
+
+#検査対象ディレクトリ名リストと最新更新時間リストをマージ
+updatetime_dir = [[] for i in range(directry_list_num)]
+
+for n in range(directry_list_num):
+    updatetime_dir[n].append(directry_list[n])
+    updatetime_dir[n].append(updatetime_new_alt[n])
+
+#検査対象ディレクトリの最新更新時間リストを出力
+for n in range(directry_list_num):
+    print ('対象：' + updatetime_dir[n][0] + ' 更新時間：' + updatetime_dir[n][1])
